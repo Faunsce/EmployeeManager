d7d4ab55d3b63424580408fa4664d1f032d57d87
Dominik Rutter <dominik.rutter@gmail.com>
2023-10-18 17:49:40 -0400

    Add project files.

---
+++ Employee Manager.cpp
@@ -0,0 +1,12 @@
+﻿// Employee Manager.cpp : Defines the entry point for the application.
+//
+
+#include "Employee Manager.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
